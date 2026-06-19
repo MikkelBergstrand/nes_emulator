d463895ec04cbbea6e317a119c6b2c0067eafada
Mikkel Bergstrand <mikkel.bergstrand@gmail.com>
2026-06-19 15:22:44 +0200

    refac ppu rendering to separate file

--- src/app.rs
+++ src/app.rs
@@ -10,11 +10,6 @@ use winit::{
     application::ApplicationHandler, event::*, event_loop::{ActiveEventLoop, EventLoop}, keyboard::{KeyCode, PhysicalKey}, window::Window
 };
 
-#[cfg(target_arch = "wasm32")]
-use wasm_bindgen::prelude::*;
-#[cfg(target_arch = "wasm32")]
-use winit::platform::web::EventLoopExtWebSys;
-
 const RESOLUTION: (usize, usize) = (256, 240);
 
 // This will store the state of our game
@@ -27,7 +22,6 @@ pub struct State {
     render_pipeline: wgpu::RenderPipeline,
     vertex_buffer: wgpu::Buffer,
     index_buffer: wgpu::Buffer,
-    num_vertices: u32,
     num_indices: u32,
     window: Arc<Window>,
 	diffuse_bind_group: wgpu::BindGroup,
@@ -46,10 +40,7 @@ impl State {
         // THe instance is a handle to our GPU
         // BackendBit::Primary => Vulkan + Metal + DX12 + Browser WebGPU
         let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
-            #[cfg(not(target_arch = "wasm32"))]
             backends: wgpu::Backends::PRIMARY,
-            #[cfg(target_arch = "wasm32")]
-            backends: wgpu::Backends::GL,
             flags: Default::default(),
             memory_budget_thresholds: Default::default(),
             backend_options: Default::default(),
@@ -214,7 +205,6 @@ impl State {
             }
         );
 
-        let num_vertices = VERTICES.len() as u32;
         let num_indices = INDICES.len() as u32;
 
         Ok(Self {
@@ -226,7 +216,6 @@ impl State {
             render_pipeline,
             vertex_buffer,
             index_buffer,
-            num_vertices,
             num_indices,
             window,
             diffuse_bind_group,
@@ -352,19 +341,13 @@ impl State {
 
 
 pub struct App {
-    #[cfg(target_arch = "wasm32")]
-    proxy: Option<winit::event_loop::EventLoopProxy<State>>,
     state: Option<State>,
 }
 
 impl App {
-    pub fn new(#[cfg(target_arch = "wasm32")] event_loop: &EventLoop<State>) -> Self {
-        #[cfg(target_arch = "wasm32")]
-        let proxy = Some(event_loop.create_proxy());
+    pub fn new() -> Self {
         Self {
             state: None,
-            #[cfg(target_arch = "wasm32")]
-            proxy,
         }
     }
 }
@@ -375,59 +358,16 @@ impl ApplicationHandler<State> for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         #[allow(unused_mut)]
         let mut window_attributes = Window::default_attributes();
-
-        #[cfg(target_arch = "wasm32")]
-        {
-            use wasm_bindgen::JsCast;
-            use winit::platform::web::WindowAttributesExtWebSys;
-            
-            const CANVAS_ID: &str = "canvas";
-
-            let window = wgpu::web_sys::window().unwrap_throw();
-            let document = window.document().unwrap_throw();
-            let canvas = document.get_element_by_id(CANVAS_ID).unwrap_throw();
-            let html_canvas_element = canvas.unchecked_into();
-            window_attributes = window_attributes.with_canvas(Some(html_canvas_element));
-        }
-
         let window = Arc::new(event_loop.create_window(window_attributes).unwrap());
 
         #[cfg(not(target_arch = "wasm32"))]
         {
-            // If we are not on web we can use pollster to
-            // await the 
             self.state = Some(pollster::block_on(State::new(window)).unwrap());
-        }
-
-        #[cfg(target_arch = "wasm32")]
-        {
-            // Run the future asynchronously and use the
-            // proxy to send the results to the event loop
-            if let Some(proxy) = self.proxy.take() {
-                wasm_bindgen_futures::spawn_local(async move {
-                    assert!(proxy
-                        .send_event(
-                            State::new(window)
-                                .await
-                                .expect("Unable to create canvas!!!")
-                        )
-                        .is_ok())
-                });
-            }
         }
     }
 
     #[allow(unused_mut)]
     fn user_event(&mut self, _event_loop: &ActiveEventLoop, mut event: State) {
-        // This is where proxy.send_event() ends up
-        #[cfg(target_arch = "wasm32")]
-        {
-            event.window.request_redraw();
-            event.resize(
-                event.window.inner_size().width,
-                event.window.inner_size().height,
-            );
-        }
         self.state = Some(event);
     }
 
@@ -475,10 +415,6 @@ pub fn run() -> anyhow::Result<()> {
     {
         env_logger::init();
     }
-    #[cfg(target_arch = "wasm32")]
-    {
-        console_log::init_with_level(log::Level::Info).unwrap_throw();
-    }
 
     let event_loop = EventLoop::with_user_event().build()?;
     #[cfg(not(target_arch = "wasm32"))]
@@ -486,20 +422,6 @@ pub fn run() -> anyhow::Result<()> {
         let mut app = App::new();
         event_loop.run_app(&mut app)?;
     }
-    #[cfg(target_arch = "wasm32")]
-    {
-        let app = App::new(&event_loop);
-        event_loop.spawn_app(app);
-    }
-
-    Ok(())
-}
-
-#[cfg(target_arch = "wasm32")]
-#[wasm_bindgen(start)]
-pub fn run_web() -> Result<(), wasm_bindgen::JsValue> {
-    console_error_panic_hook::set_once();
-    run().unwrap_throw();
 
     Ok(())
 }
--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -1,5 +1,6 @@
 pub mod pattern_table;
 mod addressing;
+mod rendering;
 
 use core::panic;
 use std::{usize};
@@ -8,10 +9,6 @@ use image::Rgb;
 
 use crate::nes_parser::NametableArrangement;
 
-const IMG_WIDTH: usize = 256;
-const IMG_HEIGHT: usize = 240;
-const BYTES_PER_PIXEL: usize = 4;
-const IMG_SIZE: usize = IMG_HEIGHT*IMG_WIDTH*BYTES_PER_PIXEL;
 
 pub struct PPU {
     color_data: [Rgb<u8>; 64],
@@ -78,7 +75,7 @@ impl PPU {
             w: false,
             color_data: color_data.try_into().unwrap(),
             addressor: addressing::PPUMemoryMap::new(chr_data, nametable_arrangement),
-            image_out: vec![0u8; IMG_SIZE],
+            image_out: vec![0u8; rendering::IMG_SIZE],
             image_ready: true,
 
             pattern_data_lb: 0,
@@ -97,7 +94,10 @@ impl PPU {
             1 => { self.mask = data; }
             2 => { self.status = data; }
             3 => { self.oam_addr = data; }
-            4 => { self.oam_data = data; }
+            4 => { 
+                self.oam_addr = self.oam_addr.wrapping_add(1);
+                self.oam_data = data; 
+            }
             5 => {  //PPUSCROLL
                 // Writes to scroll changes xscroll and yscroll
                 // depending on the order of writes.
@@ -169,165 +169,6 @@ impl PPU {
     pub fn pending_nmi(&self) -> bool { self.pending_nmi }
     pub fn clear_nmi(&mut self) { self.pending_nmi = false; self.nmi_lineout = false; }
 
-    fn inc_hori(&mut self) {
-        // X-increment (when next tile is reached)
-        if (self.v & 0x001F) == 31  {
-            self.v &= !0x001F;
-            self.v ^= 0x0400;
-        } else {
-            self.v += 1;
-        }
-
-        self.v &= 0x7FFF;
-
-    }
-
-    fn inc_vert(&mut self) {
-        if (self.v & 0x7000) != 0x7000 {
-            self.v += 0x1000;
-        } else {
-            self.v &= !0x7000;
-            let mut y = (self.v & 0x03E0) >> 5;
-            if y == 29 { 
-                y = 0;
-                self.v ^= 0x0800;
-            } else if y == 31 {
-                y = 0;
-            } else {
-                y += 1;
-            }
-            self.v = (self.v & !0x03E0) | (y << 5);
-        }
-
-        self.v &= 0x7FFF;
-    }
-
-    fn load_shifters(&mut self) {
-        let tile_address = 0x2000 | (self.v & 0x0FFF);
-        let attribute_address = 0x23C0 | (self.v & 0x0C00) 
-            | ((self.v >> 4) & 0x38) 
-            | ((self.v >> 2) & 0x07);
-
-        let fine_y = (self.v >> 12) & 0x07;
-
-        let pattern_base = self.base_background_pattern_address();
-        let tile =  self.addressor.read(tile_address) as u16;
-
-        let mut attribute = self.addressor.read(attribute_address);
-        if (self.v >> 5) & 0x02 != 0 { attribute >>= 4; }
-        if  self.v       & 0x02 != 0 { attribute >>= 2; }
-        let attribute = attribute & 0x03;
-
-        let pattern_low_addr  = pattern_base | (tile << 4) | 0 | fine_y; 
-        let pattern_high_addr = pattern_base | (tile << 4) | 8 | fine_y; 
-
-        self.pattern_data_lb = (self.pattern_data_lb & 0xFF00) | ((self.addressor.read(pattern_low_addr) as u16));
-        self.pattern_data_hb = (self.pattern_data_hb & 0xFF00) | ((self.addressor.read(pattern_high_addr) as u16));
-
-        let lo = if attribute & 0b01 != 0 { 0xFF } else { 0x00 };
-        let hi = if attribute & 0b10 != 0 { 0xFF } else { 0x00 };
-
-        self.attribute_data_lb = (self.attribute_data_lb & 0xFF00) | lo;
-        self.attribute_data_hb = (self.attribute_data_hb & 0xFF00) | hi;
-    }
-
-    fn update_shifters(&mut self)  {
-        self.pattern_data_lb <<= 1;
-        self.pattern_data_hb <<= 1;
-        self.attribute_data_lb <<= 1;
-        self.attribute_data_hb <<= 1;
-    }
-
-    fn draw_tile(&mut self, xpos: usize, ypos: usize) {
-        let bit_mux = 0x8000u16 >> self.x;
-        let p0 = ((self.pattern_data_lb & bit_mux) != 0) as u8;
-        let p1 = ((self.pattern_data_hb & bit_mux) != 0) as u8;
-        let bg_pixel = (p1 << 1) | p0;
-        
-
-        let a0 = ((self.attribute_data_lb & bit_mux) != 0) as u8;
-        let a1 = ((self.attribute_data_hb & bit_mux) != 0) as u8;
-        let attr = (a1 << 1) | a0;
-
-        let pallette_addr = if bg_pixel == 0 {
-            0x3F00
-        } else {
-            0x3F00 | ((attr as u16) << 2) | (bg_pixel as u16)
-        };
-        let mut color_index = (self.addressor.read(pallette_addr) & 0x3F) as usize;
-
-        if self.greyscale() {
-            color_index = color_index & 0x30;
-        }
-        let rgb = self.color_data[color_index];
-        
-        let output_pixel = (ypos as usize)*IMG_WIDTH + (xpos as usize);
-        self.image_out[BYTES_PER_PIXEL*output_pixel+0] = rgb[0];
-        self.image_out[BYTES_PER_PIXEL*output_pixel+1] = rgb[1];
-        self.image_out[BYTES_PER_PIXEL*output_pixel+2] = rgb[2];
-    }
-
-    pub fn tick(&mut self) {
-        if !self.enable_sprites() && !self.enable_background() {
-            // Only render if mask allows it
-            if self.scanline == 1 && self.cycle == 1 {
-                self.status |= 0x40; //DEBUG: set sprite 0 flag manually
-            }
-
-            if self.scanline == 261 && (280..=304).contains(&self.cycle){
-                // vert(v) = vert(t);
-                self.v = (self.v & !0x7BE0) | (self.t & 0x7BE0);
-            }
-
-            if (0..=239).contains(&self.scanline) || self.scanline == 261 {
-                if (2..=257).contains(&self.cycle) || (321..=337).contains(&self.cycle) {
-                    self.update_shifters();
-                }
-
-                if (2..=257).contains(&self.cycle) || (321..=337).contains(&self.cycle) {
-                    if (self.cycle - 1) % 8 == 0 { self.load_shifters(); }
-                    if (self.cycle - 1) % 8 == 7 { self.inc_hori(); }
-                }
-
-                if self.cycle == 256 {
-                    self.inc_vert();
-                }
-
-                if self.cycle == 257 { 
-                    //horiz(v) = horiz(t);
-                    self.v = (self.v & !0x041F) | (self.t & 0x041F); 
-                }
-
-                if (1..=256).contains(&self.cycle) && self.scanline < 240 {
-                    self.draw_tile(self.cycle-1, self.scanline);
-                }
-            }
-        }
-
-
-        if self.scanline == 241 && self.cycle == 1 {
-            // Set vblank flag
-            self.status |= 0x80;
-            // Must have NMI enabled in PPUCTRL
-            if self.vblank_nmi_enable() {
-                // Only trigger NMI on rising edge of STATUS bit.
-                if !self.nmi_lineout {
-                    self.pending_nmi = true;
-                }
-                self.nmi_lineout = true;
-            }
-            self.image_ready = true;
-        }
-
-        self.cycle += 1;
-        if self.cycle > 340 {
-            self.cycle = 0;
-            self.scanline += 1;
-            if self.scanline > 261 {
-                self.scanline = 0;
-            }
-        }
-    }
 
     pub fn get_image_bytes(&self) -> &Vec<u8> { return &self.image_out; }
 
